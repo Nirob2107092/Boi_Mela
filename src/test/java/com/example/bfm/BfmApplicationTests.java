461c6d641b5362ac9efa7f707a7d6faf7d2e9c0e
Rysul Nirob <rysulamannirob@gmail.com>
2026-03-01 12:26:47 +0600

    successfully created a spring boot applicaton and contenarize this using docker

---
+++ src/test/java/com/example/bfm/BfmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bfm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BfmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
